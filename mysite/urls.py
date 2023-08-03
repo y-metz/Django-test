d17fb81d0dceca0ff95d83cc6d6a7c77dd54c59e
y-metz <poohyuzuto@gmail.com>
2023-08-03 23:54:59 +0900

    change urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,11 +5,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("blog.urls")), #トップページへのリクエストはblog.urlsにリダイレクト
 ]
-
-# blog/urls.pyを作成
-from django.urls import path
-from . import views
-
-urlpatterns = {
-    path('', vuews.post_list, name='post_list'), # トップページにリクエストが来たらviews.post_listへ
-}
