a7f2d8fb903cbaa995f6cc769881dba70cdbea16
y-metz <poohyuzuto@gmail.com>
2023-08-04 00:23:15 +0900

    fix a typo

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ def post_list(request):
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return rende(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
