982fc660e792b63555fe9ed03b6fb095d13b97a9
y-metz <poohyuzuto@gmail.com>
2023-08-03 23:53:04 +0900

    remove view realted

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
-from .forms import PostForm
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
