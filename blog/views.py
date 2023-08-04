d44dc16c70b7f6469ec7e657b3510b2cb6154d12
y-metz <poohyuzuto@gmail.com>
2023-08-04 14:21:28 +0900

    feat post_list

--- blog/views.py
+++ blog/views.py
@@ -3,13 +3,13 @@ from django.utils import timezone
 from .models import Post
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_details.html', {'post': post})
+# def post_detail(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#     return render(request, 'blog/post_details.html', {'post': post})
 
-def post_new(request):
-    form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+# def post_new(request):
+#     form = PostForm()
+#     return render(request, 'blog/post_edit.html', {'form': form})
