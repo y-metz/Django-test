28c8505eaca2f912aa49d958c9c75c3ab0a36285
y-metz <poohyuzuto@gmail.com>
2023-08-04 13:55:03 +0900

    add mime css to settingw.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,3 +125,7 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# for MIME css
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
