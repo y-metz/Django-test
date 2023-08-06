cd43e325a4260e1eefeef3ce867d362c91e55a05
y-metz <poohyuzuto@gmail.com>
2023-08-06 15:26:38 +0900

    Reset STATICFILES related

--- mysite/settings.py
+++ mysite/settings.py
@@ -139,8 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = str(BASE_DIR / "staticfiles")
 # 以下を追加
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
